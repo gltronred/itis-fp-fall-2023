f1f5485d6f81d230fae6d2cbb6a4e3996f15344b
Mansur Ziiatdinov <gltronred@gmail.com>
2023-11-09 20:53:16 +0300

    Homework 0

--- p0/src/MyLib.hs
+++ p0/src/MyLib.hs
@@ -6,3 +6,19 @@ someFunc = putStrLn "someFunc"
 rev :: [a] -> [a]
 rev [] = []
 rev (x:xs) = xs -- [x]
+
+-- Напишите тесты к функции и функцию
+--
+-- Работает как zip, но если один список
+-- длиннее, циклически переиспользует второй
+--
+-- > zipLong [1,2,3] "abc"
+-- [(1,'a'),(2,'b'),(3,'c')]
+--
+-- > zipLong [1,2] "abcd"
+-- [(1,'a'),(2,'b'),(1,'c'),(2,'d')]
+--
+-- > zipLong [] "abcd"
+-- []
+zipLong :: [a] -> [b] -> [(a,b)]
+zipLong as bs = error "Implement me!"
