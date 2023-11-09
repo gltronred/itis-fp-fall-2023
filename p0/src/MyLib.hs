01952993922a8153f73b75ba0b15efea1407510a
Mansur Ziiatdinov <gltronred@gmail.com>
2023-11-09 20:45:52 +0300

    More test examples

--- p0/src/MyLib.hs
+++ p0/src/MyLib.hs
@@ -1,4 +1,8 @@
-module MyLib (someFunc) where
+module MyLib where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+rev :: [a] -> [a]
+rev [] = []
+rev (x:xs) = xs -- [x]
--- p0/test/Main.hs
+++ p0/test/Main.hs
@@ -6,7 +6,8 @@ import Test.Tasty.Hedgehog
 import Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
-import Data.List
+
+import MyLib
 
 main :: IO ()
 main = defaultMain tests
@@ -14,13 +15,30 @@ main = defaultMain tests
 prop_rev :: Property
 prop_rev = property $ do
   xs <- forAll $ Gen.list (Range.linear 0 10) Gen.alpha
-  reverse (reverse xs) === xs
+  rev (rev xs) === xs
+
+prop_opaque :: Property
+prop_opaque = property $ do
+  xs <- forAll $ Gen.list (Range.linear 0 10) Gen.alpha
+  let op = Opaque xs
+  op /== op
+
+prop_div10000 :: Property
+prop_div10000 = property $ do
+  x <- forAll $ Gen.int (Range.linear 1 100000)
+  x `mod` 10000 /== 0
 
 tests :: TestTree
 tests = testGroup "All Tests"
   [ testGroup "Unit Tests"
     [ testCase "3*5 == 15" $ 3*5 @?= 15
     , testCase "2*2 == 4" $ 4 @=? 2*2
+    , testCase "rev []" $ rev [] @?= ([] :: [Int])
+    , testCase "rev [1,2,3]" $
+      rev [1,2,3] @?= [3,2,1]
     ]
   , testProperty "reverse works" $ prop_rev
+  , testProperty "strange opaque value" prop_opaque
+  , testProperty "all numbers do not divide 10000" $
+    prop_div10000
   ]
